4cfac8eb127d6bd554cabd236d24ca61322ae3b9
Zachary Lemmon <musicmage4114@aim.com>
2017-03-18 20:22:35 +0000

    Initialize repository

---
+++ spec/esi_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe Esi do
+  it "has a version number" do
+    expect(Esi::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
